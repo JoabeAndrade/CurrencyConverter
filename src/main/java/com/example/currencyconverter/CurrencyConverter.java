893c266f8f3caf3a5ee29ae21e4e3eb206287d23
Joabe Andrade <joabeandrade2k01@gmail.com>
2023-08-07 13:38:19 -0300

    Creation of the currency converter project and implementation of libraries and main function

---
+++ src/main/java/com/example/currencyconverter/CurrencyConverter.java
@@ -0,0 +1,15 @@
+package com.example.currencyconverter;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class CurrencyConverter extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
